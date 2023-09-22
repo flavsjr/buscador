f34f555720da125f7a4bf5995045e72c5420a789
flaviano.junior <flavianogjr@gmail.com>
2023-09-22 14:32:01 -0300

    Adicionando estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Buscador de CEP</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..."/>
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+
+      </div>
+
+    <main className="main">
+      <h2>CEP: 703092831-83</h2>
+
+      <span>Rua Teste algum</span>
+      <span>Complemento: Algum Complemento</span>
+      <span>Campo Grande - MS</span>
+    </main>
+
     </div>
   );
 }
